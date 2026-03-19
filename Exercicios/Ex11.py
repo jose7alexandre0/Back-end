6aedadfccccbf9fd495f4cef4ed60042657ca4fd
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-03-19 08:51:48 -0300

    Create Ex11.py

---
+++ Exercicios/Ex11.py
@@ -0,0 +1,4 @@
+reais = float(input("Quanto dinheiro você tem na carteira? R$ "))
+cotacao_dolar = 5.24
+dolares = reais / cotacao_dolar
+print(f"Com R$ {reais:.2f}, você pode comprar US$ {dolares:.2f}")

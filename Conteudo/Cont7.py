753ec67f34dc5e1e092af56a0f905a69c2cc20c6
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-03-26 08:36:28 -0300

    Update Cont7.py

--- Conteudo/Cont7.py
+++ Conteudo/Cont7.py
@@ -5,3 +5,16 @@ Para importar coisa específicas de uma bibllioteca utiliza-se o comando from se
 BIBLIOTECA MATH
 Essa biblioteca e bem utilizada no Python e seu nome significa matemática.
 Quando importada ela traz diversos recursos matemáticos como por exemplo a funcionalidade ceil que arredonda números pra cima ou a funcionalidade floor que realiza o arredondamento pra baixo.
+BIBLIOTECA MATH
+A funcionalidade trunc, elimina números da virgula pra frente.
+A funcionalidade pow é usada para a exponenciação, semelhante ao uso de 2 asteríscos.
+A funcionalidade sqrt utilizada para calcular a raiz quadrada
+A funcionalidade factorial calcula o fatorial de um número
+Ex: import math (importa tudo para o programa).
+From math import sqrt, ceil(importa apenas a funcionalidade sqrt e a ceil).
+
+import math
+num = int(input('Digite um número: ' ))
+raiz = math.sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz))) #arredonda pra cima
+print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))

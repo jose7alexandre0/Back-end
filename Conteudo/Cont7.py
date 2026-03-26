b9b4a0e8afa20d07fb7cc587492b73524e9c3b5a
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-03-26 08:22:10 -0300

    Create Cont7.py

---
+++ Conteudo/Cont7.py
@@ -0,0 +1,7 @@
+MÓDULOS EM PYTHON
+Para importar bibliotecas e funcionalidades em Python utiliza-se o comando Import seguido do nome da biblioteca ou módulo que ira ser carregado nesse programa.
+Para importar coisa específicas de uma bibllioteca utiliza-se o comando from seguido do nome da biblioteca seguido da palavra import seguido do item que deseja importar.
+
+BIBLIOTECA MATH
+Essa biblioteca e bem utilizada no Python e seu nome significa matemática.
+Quando importada ela traz diversos recursos matemáticos como por exemplo a funcionalidade ceil que arredonda números pra cima ou a funcionalidade floor que realiza o arredondamento pra baixo.

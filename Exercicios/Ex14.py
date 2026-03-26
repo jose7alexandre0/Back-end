2c573cec4fde3ef5122b59a552036f32aa54f8f0
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-03-26 07:48:45 -0300

    Update Ex14.py

--- Exercicios/Ex14.py
+++ Exercicios/Ex14.py
@@ -2,3 +2,7 @@ Minha resposta:
 salario = float(input("Digite o salário atual: R$ "))
 novo_salario = salario * 1.15
 print(f"O novo salário com 15% de aumento é: R$ {novo_salario:.2f}")
+RESOLUÇÃO:
+salario float (input('Qual é o salário do funcionário?R$'))
+aumento salario + (salario*15/100)
+print('um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salario, aumento))

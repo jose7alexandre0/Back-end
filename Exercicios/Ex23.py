33fb89534c308f3a7980c70115a6fe110d52bd2b
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-05-07 08:12:20 -0300

    Create Ex23.py

---
+++ Exercicios/Ex23.py
@@ -0,0 +1,21 @@
+num = int(input('Digite um número entre 0 e 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milha: {m}')
+
+RESOLUÇÃO
+numero = int(input('informe um número'))
+u = numero//1 % 10
+d = numero//10 % 10
+c = numero//100% 10
+m = numero//1000% 10
+print('Analisando {}'.format(numero))
+print('unidade {}'.format(u))
+print('dezena {}'.format(d))
+print('centana {}'.format(c))
+print('milhar {}'.format(m))

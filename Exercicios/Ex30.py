f50f35c430c1d61434e65fbe7e1cf7b3f1eaae3e
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-05-13 08:07:03 -0300

    Update Ex30.py

--- Exercicios/Ex30.py
+++ Exercicios/Ex30.py
@@ -6,3 +6,12 @@ else:
     preco = distancia * 0.45
 
 print(f"O preço da sua passagem será R${preco:.2f}")
+
+Resolução
+viagem = float(input("Qual é a distância da sua viagem?"))
+curta = 0.5 * viagem
+longa = 0.45 * viagem
+print("Você está prestes a começar uma viagem de {} Km".format(viagem))
+if viagem <=200: print("E o preço da sua passagem será de R${}".format(curta))
+else:
+    print("E o preço da sua passagem será de R${:.2f}".format(longa))

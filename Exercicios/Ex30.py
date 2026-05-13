398e7990683bb4b175aa2c0d49db7a7db49253e6
jose7alexandre0 <Jose.pelisser@escola.pr.gov.br>
2026-05-13 08:03:07 -0300

    Create Ex30.py

---
+++ Exercicios/Ex30.py
@@ -0,0 +1,8 @@
+distancia = float(input("Qual é a distância da viagem em km? "))
+
+if distancia <= 200:
+    preco = distancia * 0.50
+else:
+    preco = distancia * 0.45
+
+print(f"O preço da sua passagem será R${preco:.2f}")
